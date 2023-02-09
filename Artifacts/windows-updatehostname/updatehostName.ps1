0419222744c369da5863723820d3ac0bb71bed1d
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-02-09 10:46:26 -0500

    Added artifact to update hostname

---
+++ Artifacts/windows-updatehostname/updatehostName.ps1
@@ -0,0 +1,75 @@
+[CmdletBinding()]
+param(
+    [string] $HostName
+)
+
+
+###################################################################################################
+#
+# PowerShell configurations
+#
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+# Hide any progress bars, due to downloads and installs of remote components.
+$ProgressPreference = "SilentlyContinue"
+
+# Ensure we force use of TLS 1.2 for all downloads.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Discard any collected errors from a previous execution.
+$Error.Clear()
+
+# if the HostName is empty, use %COMPUTERNAME% as the value
+if ([String]::IsNullOrWhiteSpace($HostName))
+{
+    $HostName = $env:COMPUTERNAME
+}
+
+###################################################################################################
+#
+# Handle all errors in this script.
+#
+
+trap
+{
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    #       script, unless you want to ignore a specific error.
+    $message = $Error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "`nERROR: $message" -ForegroundColor Red
+    }
+
+    Write-Host "`nThe artifact failed to apply.`n"
+
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
+
+
+###################################################################################################
+#
+# Main execution block.
+#
+
+Write-Host "Preparing to update the hostname of the machine"
+
+
+try
+{
+    Push-Location $PSScriptRoot
+
+    Rename-Computer -NewName $HostName
+
+    Write-Host "`nThe artifact was applied successfully.`n"
+}
+finally
+{
+    Pop-Location
+}
