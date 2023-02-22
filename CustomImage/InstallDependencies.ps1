78e3b5e6dcc5ae75d544aa61a4fa3a3e07573662
SrinuLekkala <Srinu.Lekkala@viacomcontractor.com>
2023-02-23 02:26:21 +0530

    updated the script

--- CustomImage/InstallDependencies.ps1
+++ CustomImage/InstallDependencies.ps1
@@ -1,6 +1,5 @@
-# Install PowerShell modules
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name PnP.PowerShell -Scope AllUsers -Repository PSGallery
-Install-Module -Name CredentialManager -Scope AllUsers -Repository PSGallery
-Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
+
+Install-Module -Name PnP.PowerShell -RequiredVersion 1.11.0
+Install-Module -Name CredentialManager
+
 
