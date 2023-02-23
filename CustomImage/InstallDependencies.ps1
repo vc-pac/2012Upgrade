ab40b766e316bcc5f6c7187a6e4d2027dfe75fa5
SrinuLekkala <Srinu.Lekkala@viacomcontractor.com>
2023-02-24 00:16:17 +0530

    updated with error handling

--- CustomImage/InstallDependencies.ps1
+++ CustomImage/InstallDependencies.ps1
@@ -1,16 +1,20 @@
+$chromeUrl = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+$chromeInstaller = "$env:TEMP\ChromeInstaller.exe"
+Invoke-WebRequest -Uri $chromeUrl -OutFile $chromeInstaller
 
-    $chromeUrl = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
-    $chromeInstaller = "$env:TEMP\ChromeInstaller.exe"
-    Invoke-WebRequest -Uri $chromeUrl -OutFile $chromeInstaller
-    
-    # Install Chrome silently
-    $installArgs = "/silent /install"
-    $process = Start-Process -FilePath $chromeInstaller -ArgumentList $installArgs -Wait
+# Check if the download was successful
+if (-not (Test-Path $chromeInstaller)) {
+    Write-Error "Failed to download Google Chrome installer"
+    exit 1
+}
 
-    if ($process.ExitCode -eq 0) {
-        Write-Host "Google Chrome has been successfully installed successfully"
-    }
+# Install Chrome silently
+$installArgs = "/silent /install"
+$process = Start-Process -FilePath $chromeInstaller -ArgumentList $installArgs -Wait
 
-    else {
-        Write-Host "Google Chrome not installed"
-    }
+if ($process.ExitCode -eq 0) {
+    Write-Host "Google Chrome has been successfully installed"
+} else {
+    Write-Error "Failed to install Google Chrome with exit code $($process.ExitCode)"
+    exit 1
+}
