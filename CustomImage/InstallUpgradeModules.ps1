cb4370f5684bef5def25c283e42cc5d47d09296a
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-02-23 11:45:45 -0500

    Update InstallUpgradeModules.ps1

--- CustomImage/InstallUpgradeModules.ps1
+++ CustomImage/InstallUpgradeModules.ps1
@@ -1,3 +1,7 @@
+Write-Host "Installing module PowerShellGet..."
+Install-Module -Name PowerShellGet -RequiredVersion 1.6.5 -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
 Write-Host "Installing module CredentialManager ..."
 Install-Module -Name CredentialManager
 Write-Host "Installed module CredentialManager"
