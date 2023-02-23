59182e65ea84f9406862d46a5d141dc375832327
SrinuLekkala <Srinu.Lekkala@viacomcontractor.com>
2023-02-23 20:33:33 +0530

    updated the script

--- CustomImage/InstallDependencies.ps1
+++ CustomImage/InstallDependencies.ps1
@@ -1,36 +1,23 @@
+# Install specific versions of PowerShell modules from the PowerShell Gallery
+$modules = @(
+    @{ Name = "Az"; Version = "2.0.0" },
+    @{ Name = "Az.Accounts"; Version = "1.5.2" },
+    @{ Name = "AzureAD"; Version = "2.0.2.130" },
+    @{ Name = "CredentialManager"; Version = "2.0.0" },
+    @{ Name = "ExchangeOnlineManagement"; Version = "2.0.5" },
+    @{ Name = "Microsoft.Graph"; Version = "1.7.0" },
+    @{ Name = "MicrosoftTeams"; Version = "4.4.1" },
+    @{ Name = "SharePointPnPPowerShell2013"; Version = "3.26.2010.0" },
+    @{ Name = "SharePointPnPPowerShellOnline"; Version = "3.25.2009.1" }
+)
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-
-try {
-    # Install PnP.PowerShell module
-    If(Find-Module PnP.PowerShell -RequiredVersion 1.11.0){
-    Write-Host "Found pnp.powershell version 1.11.0 in PSGallery"
-    Write-Host "Installing PnP.PowerShell module..."
-    Install-Module -Name PnP.PowerShell -RequiredVersion 1.11.0 -Force -ErrorAction Stop
-
-    # Log successful installation
-    Write-Host "PnP.PowerShell module installed successfully."
+foreach ($module in $modules) {
+    $moduleName = $module.Name
+    $moduleVersion = $module.Version
+    if (-not (Get-Module -Name $moduleName -ErrorAction SilentlyContinue)) {
+        Write-Host "Installing module $moduleName version $moduleVersion ..."
+        Install-Module -Name $moduleName -RequiredVersion $moduleVersion -Force
+    } else {
+        Write-Host "Module $moduleName version $moduleVersion is already installed."
     }
-    elseif(Find-Module CredentialManager){
-    Write-Host "Found CredentialManager in PSGallery"
-
-    # Install CredentialManager module
-    Write-Host "Installing CredentialManager module..."
-    Install-Module -Name CredentialManager -Force -ErrorAction Stop
-
-    # Log successful installation
-    Write-Host "CredentialManager module installed successfully."
-    }
-    Else{
-    Write-Host "No internet connectivity"
-    }
-}
-
-catch {
-    # Log error message
-    Write-Host "Error occurred during module installation:`n$($_.Exception.Message)"
-
-    # Throw exception to terminate script
-    throw $_.Exception
 }
