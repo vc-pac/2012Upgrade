cd0d76c81f4c7148d6fb35260ae50c288e0f32c9
SrinuLekkala <Srinu.Lekkala@viacomcontractor.com>
2023-02-23 23:47:26 +0530

    chrome installer added

--- CustomImage/InstallDependencies.ps1
+++ CustomImage/InstallDependencies.ps1
@@ -1,23 +1,16 @@
-# Install specific versions of PowerShell modules from the PowerShell Gallery
-$modules = @(
-    @{ Name = "Az"; Version = "2.0.0" },
-    @{ Name = "Az.Accounts"; Version = "1.5.2" },
-    @{ Name = "AzureAD"; Version = "2.0.2.130" },
-    @{ Name = "CredentialManager"; Version = "2.0.0" },
-    @{ Name = "ExchangeOnlineManagement"; Version = "2.0.5" },
-    @{ Name = "Microsoft.Graph"; Version = "1.7.0" },
-    @{ Name = "MicrosoftTeams"; Version = "4.4.1" },
-    @{ Name = "SharePointPnPPowerShell2013"; Version = "3.26.2010.0" },
-    @{ Name = "SharePointPnPPowerShellOnline"; Version = "3.25.2009.1" }
-)
 
-foreach ($module in $modules) {
-    $moduleName = $module.Name
-    $moduleVersion = $module.Version
-    if (-not (Get-Module -Name $moduleName -ErrorAction SilentlyContinue)) {
-        Write-Host "Installing module $moduleName version $moduleVersion ..."
-        Install-Module -Name $moduleName -RequiredVersion $moduleVersion -Force
-    } else {
-        Write-Host "Module $moduleName version $moduleVersion is already installed."
+    $chromeUrl = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+    $chromeInstaller = "$env:TEMP\ChromeInstaller.exe"
+    Invoke-WebRequest -Uri $chromeUrl -OutFile $chromeInstaller
+    
+    # Install Chrome silently
+    $installArgs = "/silent /install"
+    $process = Start-Process -FilePath $chromeInstaller -ArgumentList $installArgs -Wait
+
+    if ($process.ExitCode -eq 0) {
+        Write-Host "Google Chrome has been successfully installed successfully"
     }
-}
+
+    else {
+        Write-Host "Google Chrome not installed"
+    }
