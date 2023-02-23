0c4d7ec68a1ac476e0f2979ef8f38e7c4ad257d3
SrinuLekkala <Srinu.Lekkala@viacomcontractor.com>
2023-02-24 02:13:55 +0530

    node js msi install snippet added

--- CustomImage/InstallDependencies.ps1
+++ CustomImage/InstallDependencies.ps1
@@ -1,20 +1,23 @@
-$chromeUrl = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
-$chromeInstaller = "$env:TEMP\ChromeInstaller.exe"
-Invoke-WebRequest -Uri $chromeUrl -OutFile $chromeInstaller
-
-# Check if the download was successful
-if (-not (Test-Path $chromeInstaller)) {
-    Write-Error "Failed to download Google Chrome installer"
-    exit 1
-}
-
-# Install Chrome silently
-$installArgs = "/silent /install"
-$process = Start-Process -FilePath $chromeInstaller -ArgumentList $installArgs -Wait
-
-if ($process.ExitCode -eq 0) {
-    Write-Host "Google Chrome has been successfully installed"
-} else {
-    Write-Error "Failed to install Google Chrome with exit code $($process.ExitCode)"
-    exit 1
-}
+try{
+    $nodeUrl = "https://nodejs.org/dist/v12.14.1/node-v12.14.1-x64.msi"
+    $nodeInstaller = "$env:TEMP\node-v12.14.1-x64.msi"
+    Invoke-WebRequest -Uri $nodeUrl -OutFile $nodeInstaller
+    
+    $installArgs = "/qn /i '$nodeInstaller'"
+    
+    # Install Node.js
+    #$process = Start-Process -FilePath msiexec.exe -ArgumentList $installArgs -Wait -Passthru
+    
+    $process = Start-Process $nodeInstaller -Wait -ArgumentList "/quiet /passive" -PassThru
+    
+    # Check if Node.js was installed successfully
+    if ($process.ExitCode -eq 0) {
+        Write-Host "Node.js version 12.14.1 has been installed successfully"
+    }
+    else {
+        Write-Host "Node.js installation failed"
+    }
+    }
+    catch{
+        Write-Host "An exception occurred: $_Exception.message"
+    }
