77dcd7bd34af590c9e678f66610c17f0166b0918
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-02-24 15:10:44 -0500

    Update InstallUpgradeModules.ps1

--- CustomImage/InstallUpgradeModules.ps1
+++ CustomImage/InstallUpgradeModules.ps1
@@ -1,7 +1,22 @@
-Write-Host "Installing module PowerShellGet..."
-Install-Module -Name PowerShellGet -RequiredVersion 1.6.5 -Force
+try
+{
+  Write-Host "Installing module PowerShellGet..."
+Install-Module -Name PowerShellGet -RequiredVersion 1.6.5 -Force -ErrorAction Stop
+}
+catch
+{
+  Write-Output "Error Details: $($_.Exception.Message)"
+}
+
+try
+{
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 Write-Host "Installing module CredentialManager ..."
 Install-Module -Name CredentialManager
 Write-Host "Installed module CredentialManager"
+}
+catch
+{
+  Write-Output "Error Details: $($_.Exception.Message)"
+}
