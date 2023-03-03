4005483264f866075c1033f53aa355ab1e139055
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-03-03 12:15:42 -0500

    Update AddUpdateSysVariables.ps1

--- CustomImage/AddUpdateSysVariables.ps1
+++ CustomImage/AddUpdateSysVariables.ps1
@@ -1,3 +1,3 @@
 
-   [Environment]::SetEnvironmentVariable("PATH", "value22", [System.EnvironmentVariableTarget]::Machine)
+   [Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\cli", [System.EnvironmentVariableTarget]::Machine)
    [Environment]::SetEnvironmentVariable("ENV_1", "value11", [System.EnvironmentVariableTarget]::Machine)
