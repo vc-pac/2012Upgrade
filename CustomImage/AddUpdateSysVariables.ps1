ba92b50cc6a0696c44f1a948f4dfb6114da536f1
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-03-03 14:01:10 -0500

    Update AddUpdateSysVariables.ps1

--- CustomImage/AddUpdateSysVariables.ps1
+++ CustomImage/AddUpdateSysVariables.ps1
@@ -3,6 +3,32 @@ Param(
     [string]$Password,
     [string] $UserDomain
 )
-{
-    [Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\TerraForms;C:\Program Files\Node", [System.EnvironmentVariableTarget]::Machine)
+ 
+[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\CLI;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
+$domainuser = "$UserDomain\$Myuser"
+write-output("Checking user account ...")
+$hostname = $([Environment]::MachineName).tolower()
+$domainname = $([Environment]::UserDomainName).tolower()
+$thisuser = $([Environment]::UserName).tolower()
+
+write-output("- Current user is ""$domainname\$thisuser"" on ""$hostname"".")
+write-output("- Want to be user ""$UserName"".")
+   
+$securePwd = ConvertTo-SecureString $Password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $domainuser, $securePwd
+
+$command = { cmdkey /generic:O365 /user:spo_farm2@viacom.com /pass:Pass1
+    cmdkey /generic:2013farm /user:mtvn\sp_prd13_farm /pass:Pass2
+    cmdkey /generic:O365Viacom /user:spo_farm2@viacom.com /pass:Pass3
+
+    [Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\Terraforms;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
 }
+
+try {
+    Start-Process powershell -Credential $credential -ArgumentList "-noexit -command & {$command}"
+    write-output("New process started.")
+}
+catch {
+    write-output("New process failed to start.")
+}
+write-output("Run-once script on local VM finished.")
