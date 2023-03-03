066939b06a73d3afe70a15a853894251e649d70d
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-03-03 14:53:24 -0500

    Update AddUpdateSysVariables.ps1

--- CustomImage/AddUpdateSysVariables.ps1
+++ CustomImage/AddUpdateSysVariables.ps1
@@ -4,15 +4,15 @@ Param(
     [string] $UserDomain
 )
  
-[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\CLI;C:\Program Files\Node", [System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\CLI;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
 $domainuser = "$UserDomain\$UserName"
 write-output("Checking user account ...")
 $hostname = $([Environment]::MachineName).tolower()
 $domainname = $([Environment]::UserDomainName).tolower()
 $thisuser = $([Environment]::UserName).tolower()
 
-write-output("- Current user is ""$domainname\$thisuser"" on ""$hostname"".")
-write-output("- Want to be user ""$UserName"".")
+write-output("Current user is ""$domainname\$thisuser"" on ""$hostname"".")
+write-output("Want to execute script when logged in as ""$UserName"".")
    
 $securePwd = ConvertTo-SecureString $Password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $domainuser, $securePwd
@@ -20,12 +20,10 @@ $credential = New-Object System.Management.Automation.PSCredential $domainuser, 
 $command = { cmdkey /generic:O365 /user:spo_farm2@viacom.com /pass:Pass1
     cmdkey /generic:2013farm /user:mtvn\sp_prd13_farm /pass:Pass2
     cmdkey /generic:O365Viacom /user:spo_farm2@viacom.com /pass:Pass3
-
-    [Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\Terraforms;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
 }
 
 try {
-    Start-Process powershell -Credential $credential -ArgumentList "-noexit -command & {$command}"
+    Start-Process powershell -Credential $credential -ArgumentList "-noexit -command & {$command}" -Wait
     write-output("New process started.")
 }
 catch {
