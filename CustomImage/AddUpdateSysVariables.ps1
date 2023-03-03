9959df0f7b268a27507dcf701c93c8eaee47e749
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-03-03 15:55:22 -0500

    Update AddUpdateSysVariables.ps1

--- CustomImage/AddUpdateSysVariables.ps1
+++ CustomImage/AddUpdateSysVariables.ps1
@@ -4,9 +4,10 @@ Param(
     [string] $UserDomain
 )
  
-[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\CLI;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\Terraforms;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
+
 $domainuser = "$UserDomain\$UserName"
-write-output("Checking user account ...")
+write-output("Validating user account")
 $hostname = $([Environment]::MachineName).tolower()
 $domainname = $([Environment]::UserDomainName).tolower()
 $thisuser = $([Environment]::UserName).tolower()
@@ -17,11 +18,10 @@ write-output("Want to execute script when logged in as ""$UserName"".")
 $securePwd = ConvertTo-SecureString $Password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $domainuser, $securePwd
 
-$command = { cmdkey /generic:O365 /user:spo_farm2@viacom.com /pass:Pass1
-    cmdkey /generic:2013farm /user:mtvn\sp_prd13_farm /pass:Pass2
-    cmdkey /generic:O365Viacom /user:spo_farm2@viacom.com /pass:Pass3
+$command = { C:\Windows\system32\cmdkey.exe /generic:O365 /user:spo_farm2@viacom.com /pass:Pass1
+    C:\Windows\system32\cmdkey.exe /generic:2013farm /user:mtvn\sp_prd13_farm /pass:Pass2
+    C:\Windows\system32\cmdkey.exe /generic:O365Viacom /user:spo_farm2@viacom.com /pass:Pass3
 }
-
 try {
     Start-Process powershell -Credential $credential -ArgumentList "-noexit -command & {$command}" -Wait
     write-output("New process started.")
