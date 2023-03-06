451c7058c98484f1e39eb5c3c9b54acaa3b60f7b
SrinuLekkala <Srinu.Lekkala@viacomcontractor.com>
2023-03-06 15:30:36 +0530

    updated addupdatesysvariables script

--- CustomImage/AddUpdateSysVariables.ps1
+++ CustomImage/AddUpdateSysVariables.ps1
@@ -3,30 +3,33 @@ Param(
     [string]$Password,
     [string] $UserDomain
 )
-{
-    
-    $domainuser="$UserDomain\$Username"
-    write-output("Checking user account ...")
-    $hostname = $([Environment]::MachineName).tolower()
-    $domainname = $([Environment]::UserDomainName).tolower()
-    $thisuser = $([Environment]::UserName).tolower()
+ 
+[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\Terraforms;C:\Program Files\NodeJS;C:\Program Files\Git", [System.EnvironmentVariableTarget]::Machine)
 
-    write-output("- Current user is ""$domainname\$thisuser"" on ""$hostname"".")
-    write-output("- Want to be user ""$UserName"".")
+$domainuser = "$UserDomain\$UserName"
+write-output("Validating user account")
+$hostname = $([Environment]::MachineName).tolower()
+$domainname = $([Environment]::UserDomainName).tolower()
+$thisuser = $([Environment]::UserName).tolower()
+
+write-output("Current user is ""$domainname\$thisuser"" on ""$hostname"".")
+write-output("Want to execute script when logged in as ""$UserName"".")
    
-    $securePwd = ConvertTo-SecureString $Password -AsPlainText -Force
-    $credential = New-Object System.Management.Automation.PSCredential $domainuser, $securePwd
+$securePwd = ConvertTo-SecureString $Password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $domainuser, $securePwd
 
-            $command = {cmdkey /generic:O365 /user:spo_farm2@viacom.com /pass:Pass1
-cmdkey /generic:2013farm /user:mtvn\sp_prd13_farm /pass:Pass2
-cmdkey /generic:O365Viacom /user:spo_farm2@viacom.com /pass:Pass3}
-
-            try {
-                Start-Process powershell -Credential $credential -ArgumentList "-noexit -command & {$command}"
-                write-output("    - New process started.")
-            } catch {
-                write-output("    - New process failed to start.")
-            }
-            write-output("Run-once script on local VM finished.")
-
+$command = { C:\Windows\system32\cmdkey.exe /generic:O365 /user:spo_farm2@viacom.com /pass:Pass1
+    C:\Windows\system32\cmdkey.exe /generic:2013farm /user:mtvn\sp_prd13_farm /pass:Pass2
+    C:\Windows\system32\cmdkey.exe /generic:O365Viacom /user:spo_farm2@viacom.com /pass:Pass3
 }
+try {
+    $job = Start-Job -ScriptBlock $command -Credential $credential
+
+    Write-Output $job.ChildJobs[0].JobStateInfo.Reason.Message
+    Write-Output $job.ChildJobs[0].Error
+    write-output("New process started.")
+}
+catch {
+    write-output("New process failed to start.")
+}
+write-output("Run-once script on local VM finished.")
