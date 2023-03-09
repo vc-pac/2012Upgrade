3358a9ea74571fd8d2e5cc45a050bf926eb3fee2
deepmalamittal <deepmala.mittal@viacomcontractor.com>
2023-03-09 12:19:02 -0500

    Create ExecuteCredentialJob.ps1

---
+++ CustomImage/ExecuteCredentialJob.ps1
@@ -0,0 +1,60 @@
+Param(
+    [string]$UserName, 
+    [string]$Password,
+    [string] $UserDomain
+)
+ 
+[Environment]::SetEnvironmentVariable("PATH", "C:\Program Files\Terraforms;C:\Program Files\NodeJS", [System.EnvironmentVariableTarget]::Machine)
+
+$domainuser = "$UserDomain\$UserName"
+write-output("Validating user account")
+$hostname = $([Environment]::MachineName).tolower()
+$domainname = $([Environment]::UserDomainName).tolower()
+$thisuser = $([Environment]::UserName).tolower()
+
+write-output("Current user is ""$domainname\$thisuser"" on ""$hostname"".")
+write-output("Want to execute script when logged in as ""$UserName"".")
+   
+$securePwd = ConvertTo-SecureString $Password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $domainuser, $securePwd
+
+$taskName = "Add credentials"
+$datetimeStamp = Get-Date -Format "ddMMMyyyyHHmmss"
+    $CredsFileName = "Creds_" + $datetimeStamp + ".ps1"
+    $credsFilePath = Join-Path (Get-Location) -ChildPath $CredsFileName
+    $newItem = New-Item -Path $credsFilePath -Force -ItemType File
+
+    Add-Content -Path $credsFilePath "C:\Windows\system32\cmdkey.exe /generic:O365 /user:test@test.com /pass:Pass1"
+    Add-Content -Path $credsFilePath "C:\Windows\system32\cmdkey.exe /generic:2013farm /user:abc\test1 /pass:Pass2"
+    
+    write-output("Creds File Path is ""$credsFilePath"".")
+
+
+try {
+
+Write-Host " @ Let's look at running account of Add credentials PowerShell job"
+$task = Get-ScheduledTask -TaskName "Add credentials"
+write-output("Before update running account is ")
+write-output($task.Principal.UserId) 
+
+$someResult = Set-ScheduledTask -TaskName "Add credentials" -User $domainuser -Password $Password -TaskPath "\Microsoft\Windows\PowerShell\ScheduledJobs"
+
+Write-Host " @ Let's look at running account of Add credentials PowerShell job"
+write-output("After update running account is ")
+write-output($task.Principal.UserId)
+
+Write-Host " @ Let's start ""$taskName"" manually"
+Start-Job -DefinitionName 'Add credentials' | Format-Table
+
+Write-Host " @ Let's proof that ""$taskName"" PowerShell job has been launched"; Write-Host;
+Start-Sleep -Seconds 3
+Receive-Job -Name $taskName
+Write-Host;
+
+
+}
+catch {
+    write-output("New process failed to start.")
+    Write-Output $Error
+}
+write-output("Run-once script on local VM finished.")
